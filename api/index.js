659a3ca2c316fe735f24b925ab32b26233b7febc
dhingra-madhav <21BCS10568@cuchd.in>
2024-06-12 13:47:46 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,3 +24,4 @@ app.listen(port, () => {
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
